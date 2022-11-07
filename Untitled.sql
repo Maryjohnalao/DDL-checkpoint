4c0ca56aa7ae68a60f738fbec82e9d325ba403f3
Maryjohnalao <113846480+Maryjohnalao@users.noreply.github.com>
2022-11-07 14:10:44 +0100

    DATA DEFINITION LANGUAGE SQL

---
+++ Untitled.sql
@@ -0,0 +1,25 @@
+create table customer (
+customer_id varchar(20) primary key,
+	customer_name varchar(20) not null,
+	customer_tel int
+);
+
+
+create table product(
+product_id varchar(20) primary key,
+	product_name varchar(20) not null,
+	price int
+);
+
+
+
+create table orders(
+customer_id varchar(20),
+	product_id varchar(20),
+	quantity int,
+constraint fk_customer_id Foreign key(customer_id) references customer(customer_id),
+constraint fk_product_id Foreign key(product_id) references product(product_id)
+);
+
+
+alter table orders add column orderdate date default current_date
