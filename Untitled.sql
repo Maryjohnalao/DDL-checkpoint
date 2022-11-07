e29fd543dafb22329dd1d7188978d023337c05b2
Maryjohnalao <113846480+Maryjohnalao@users.noreply.github.com>
2022-11-07 14:18:57 +0100

    Update Untitled.sql

--- Untitled.sql
+++ Untitled.sql
@@ -1,24 +1,24 @@
 create table customer (
-customer_id varchar(20) primary key,
-	customer_name varchar(20) not null,
-	customer_tel int
+	customer_id varchar(20) primary key,
+	 customer_name varchar(20) not null,
+	  customer_tel int
 );
 
 
 create table product(
-product_id varchar(20) primary key,
-	product_name varchar(20) not null,
-	price int
+	product_id varchar(20) primary key,
+	 product_name varchar(20) not null,
+	  price int
 );
 
 
 
 create table orders(
-customer_id varchar(20),
-	product_id varchar(20),
-	quantity int,
-constraint fk_customer_id Foreign key(customer_id) references customer(customer_id),
-constraint fk_product_id Foreign key(product_id) references product(product_id)
+	customer_id varchar(20),
+	 product_id varchar(20),
+	  quantity int,
+           constraint fk_customer_id Foreign key(customer_id) references customer(customer_id),
+            constraint fk_product_id Foreign key(product_id) references product(product_id)
 );
 
 alter table product add column category varchar(20);
